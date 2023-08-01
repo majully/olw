e9221009c762dc9712bed5446a9b1eae7f749bd8
Wesley Majully <wesleymajully@hotmail.com>
2023-08-01 17:36:37 -0300

    Criação das migrations

--- database/migrations/2023_08_01_181459_create_clients_table.php
+++ database/migrations/2023_08_01_181459_create_clients_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('address_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clients');
     }
 };
